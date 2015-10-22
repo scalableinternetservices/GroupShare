8c2249d9aded6a5dc2a1dec9db3fb92fe3e5b7d7
Tanuj Mittal <tanuj.183@gmail.com>
2015-10-21 21:23:34 -0700

    Add UI to add content

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,5 +1,13 @@
 class ContentController < ApplicationController
   def create
+    @content = Content.new
+
+    @content.title = params[:content][:data]
+    @content.stream_id = params[:stream_id]
+    @content.save
+
+    @stream = Stream.find(params[:stream_id])
+    redirect_to @stream
   end
 
   def show
