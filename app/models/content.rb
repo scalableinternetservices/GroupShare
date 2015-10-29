1b0c8f658ab70daafeef8a8f8fdd42e34e18d0e4
Tanuj Mittal <tanuj.183@gmail.com>
2015-10-29 00:18:39 -0700

    Some style changes

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ActiveRecord::Base
   belongs_to :stream
   has_attached_file :file
-  validates_attachment :file, content_type: {content_type: "image/png"}
+  validates_attachment :file, content_type: {content_type: ["image/png", "image/jpeg", "image/jpg"]}
 end
