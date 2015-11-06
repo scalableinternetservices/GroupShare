88df26eba45621c861ff2197f382c965a3872b10
Tanuj Mittal <tanuj.183@gmail.com>
2015-11-05 17:07:47 -0800

    Some search updates

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,12 +1,8 @@
 class Stream < ActiveRecord::Base
   has_many :contents
+
   def self.search(q)
-    if q
-      #find(:all, :conditions => ['title LIKE ?', "%#{q}%"])
-      Stream.where('title LIKE ? OR description LIKE ?', '%'+q.to_s+'%', '%'+q.to_s+'%')
-      #else
-      #  where('')
-    end
+    Stream.where('expire_at > ? and (title LIKE ? OR description LIKE ?)', Time.now, '%'+q.to_s+'%', '%'+q.to_s+'%') if q
   end
 
 end
