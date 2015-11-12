a245f697257d084f3275889222d416dd1b3a2d9d
Tanuj Mittal <tanuj.183@gmail.com>
2015-11-12 00:48:00 -0800

    prepare for testing

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Define logger
+Rails.logger = Logger.new(STDOUT)
+
 # Initialize the Rails application.
 Rails.application.initialize!
