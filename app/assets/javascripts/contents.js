76862f5e0a2780ce6b70c62ee269f57bdfc6193e
Tanuj Mittal <tanuj.183@gmail.com>
2015-11-18 00:07:33 -0800

    Fix jquery mess

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -8,10 +8,18 @@ $(function () {
 
 function updateContents() {
     var contents = $(".contents");
-    if(contents.length == 0)  return;
+    if (contents.length == 0)  return;
 
     var stream_id = contents.attr("data-id");
     var after = $(".content:last-child").attr("data-time");
     $.getScript(stream_id + "/contents.js?after=" + after);
     setTimeout(updateContents, 5000);
 }
+
+$(document).on('change', '.btn-file :file', function () {
+    console.log("method called");
+    var input = $(this);
+    var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    $("input[name='content[data]']").val(label);
+    $("input[type='submit']").focus();
+});
