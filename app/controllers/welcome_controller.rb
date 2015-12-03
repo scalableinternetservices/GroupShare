113ed7df4de0709ff04ccb41ebecf51135255a4a
Sachin Rathod <rathodsachin20@gmail.com>
2015-12-03 02:31:37 -0800

    single group by query to reduce db count queries.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     if stale?([Stream.all, Content.all])
-      @recent_streams = Stream.where("expire_at > ?", Time.now).order(created_at: :desc).limit(10)
+      @recent_streams = Stream.where('streams.expire_at > ?', Time.now).order(created_at: :desc).limit(10)
+      @recent_streams_content_size = @recent_streams.joins('LEFT OUTER JOIN contents ON contents.stream_id=streams.id').
+          select('streams.title, streams.owner,  streams.id, streams.created_at, count(contents.id) as csize').group('streams.id', 'streams.created_at')
+
     end
   end
 end
