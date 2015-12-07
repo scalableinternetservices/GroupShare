50f349837d2eae5190ffaf37699d16f64e17e270
Tanuj Mittal <tanuj.183@gmail.com>
2015-12-07 00:47:18 -0800

    Caching and other optimizations

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'fog'
 
 gem 'chronic'
 
+gem 'dalli'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    if stale?([Stream.all, Content.all])
-      @recent_streams = Stream.where("expire_at > ?", Time.now).order(created_at: :desc).limit(10)
+    @streams = Stream.where("expire_at > ?", Time.now).includes(:contents).order(created_at: :desc).limit(10)
+    @contents = Array.new
+    @streams.each do |stream|
+      @contents.append(stream.contents)
     end
+    fresh_when([@streams, @contents])
   end
 end
