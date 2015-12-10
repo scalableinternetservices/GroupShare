8c47a39d815753a20158288a84da7696790f361f
Tanuj Mittal <tanuj.183@gmail.com>
2015-12-10 15:51:41 -0800

    Remove S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,14 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
-      :storage => :fog,
-      :fog_credentials => {
-          :use_iam_profile => true,
-          :provider => 'AWS',
-          :region => 'us-east-1'
-      },
-      :fog_directory => 'cs290b',
-      :path => 'groupshare/'
-  }
 end
